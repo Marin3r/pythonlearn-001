1eda62e223351f72ca737a678e0193ab7fc3baf5
Marin3r <vascodegouveia@yahoo.co.uk>
2014-07-04 19:36:46 +0100

    Update Assignment 5.2.py

--- week5/Assignment 5.2.py
+++ week5/Assignment 5.2.py
@@ -1,6 +1,5 @@
 """
 5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. Once 'done' is entered, print out the largest and smallest of the numbers. If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. Enter the numbers from the book for problem 5.1 and Match the sample output below. 
-
 """
 
 #output:
@@ -18,7 +17,7 @@ while True:
         n = int(num)
         largest = num if largest<num or largest == None else largest
         smallest = num if smallest>num or smallest == None else smallest
-    except:
+    except ValueError:
         print "Invalid input"
     #print num
 
